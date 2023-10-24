aae20ed92cf6035b1662d8c49e444f909afa78e9
강현곤 <shoot_1@naver.com>
2023-10-24 17:48:51 +0900

    Fix: 스크래핑 코드 수정중..
    Former-commit-id: a5be2861af0b6bc6c824c488cfa5a1a7a117a524

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38b80889d2480c1e9010647915876b0ee2413df242881ed1e7a321eb1a2101c3
-size 3768
+oid sha256:10eebeb3871631f91785040b80f3b718a445b85a7fae6adb50eb507d1ad9c386
+size 4549
