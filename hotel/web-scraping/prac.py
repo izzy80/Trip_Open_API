c3f9bc8e110f6d9c42116026c75609c1c7da81de
강현곤 <shoot_1@naver.com>
2023-10-25 01:58:40 +0900

    feat: 호텔 스크래핑 -이름에 뱃지제거, 가격,주소,위경도, 이미지
    Former-commit-id: 4e7c4954bb2ab298ba93a998bf60fbb2939072d1

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10eebeb3871631f91785040b80f3b718a445b85a7fae6adb50eb507d1ad9c386
-size 4549
+oid sha256:530ade6c8011beab36d9b5dd0ae088b945be917d4fb41be2a5db7a000a4dd3e0
+size 9409
