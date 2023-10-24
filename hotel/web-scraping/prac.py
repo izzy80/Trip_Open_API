dc5ac3edc66103f3ab48c803e8ee8efcbdc4958a
강현곤 <shoot_1@naver.com>
2023-10-24 16:40:57 +0900

    feat: 숙소 이름만 스크래핑
    Former-commit-id: 0353aca4aa8a143c3071d43e2e384972ffb213f3

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe1d499c964ce11238558c868d84e992ab9b2c183db70a43aaebb0319bb0af20
-size 3092
+oid sha256:38b80889d2480c1e9010647915876b0ee2413df242881ed1e7a321eb1a2101c3
+size 3768
