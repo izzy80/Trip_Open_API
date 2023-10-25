1170d852e89c79f99f44f2cbf094f1e80991c55e
강현곤 <shoot_1@naver.com>
2023-10-25 15:15:19 +0900

    feat: selenium 화면크기 최대로 돌아가지는버전
    Former-commit-id: a4e8b488d9b9ba5d88769b61751733eb51075535

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5008b49913413f8ab739d384b1f5dc4d97e21ef574f5bbf31a919cb269fe9f5c
-size 10160
+oid sha256:cf6dbab8798db081a09ddb3eea07ef01d373021e0630fbe6e6903fc862fde672
+size 10251
