6ae0eee354506e4d6b77274524acd101ea29b6a0
강현곤 <shoot_1@naver.com>
2023-10-26 14:44:53 +0900

    feat: 로컬에선 멀쩡한 버전아니야
    Former-commit-id: d2089ba3b2bcf0b139ef3c741fa23dbc902dac62

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac845c0d992045b43b4f36cacd99696b74bc3ae15bb7d9ae3327a72d45942352
-size 11408
+oid sha256:506c8e572993bf9f4bfe49d06f29e6db1e785f6c0afd26f0ee7f3b6ca781a2e1
+size 11557
