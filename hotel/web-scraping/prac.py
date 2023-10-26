a4e3a389e69c09cc7ec138de5d165cf74b3ddb50
강현곤 <shoot_1@naver.com>
2023-10-26 15:05:35 +0900

    feat: 스크롤링 LINUX (날짜 빼버림)
    Former-commit-id: f4e1831251cb37bd65d206f4bf567892eceb3f28

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:506c8e572993bf9f4bfe49d06f29e6db1e785f6c0afd26f0ee7f3b6ca781a2e1
-size 11557
+oid sha256:3d188c37a2844579a16e1042c15c3578aea337c5fce60b56ec2b81912101147d
+size 11030
