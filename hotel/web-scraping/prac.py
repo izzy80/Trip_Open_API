89e09f416e95e1b8881ff44cb654cef9be28892f
강현곤 <shoot_1@naver.com>
2023-10-26 17:52:12 +0900

    feat: 스크롤링 LINUX (add index.txt )
    Former-commit-id: fc028200197aeb8588f6532d3fbebbfcd88097eb

--- hotel/web-scraping/prac.py
+++ hotel/web-scraping/prac.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7702f90bad6d82c03f3c1bc9f05f3dd657594f7e5beedd0a9cb122f987ec1a8
-size 11150
+oid sha256:794824bc81c445c2c05e7ba68c760748179f9a4f203498977214755df8b27510
+size 11299
