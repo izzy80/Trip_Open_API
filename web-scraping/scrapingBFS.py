66d4b1cc987d85e3d94fbab1829c538fd9341891
홍유빈 <acrow0330@naver.com>
2023-10-27 17:53:25 +0900

    fix: 문자 입력 수정
    Former-commit-id: 25f1c040d7dda88e0a1f00ad89b4708bdcb76565

--- web-scraping/scrapingBFS.py
+++ web-scraping/scrapingBFS.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee4acf3817f68514b4dba932c7384e454daab355be8a927b2ae1afebf5d150bb
-size 4508
+oid sha256:3f0889a8286efd441c80686b03fe65c0a7d6644e2b2528b3515f952a7f99c6a4
+size 4522
