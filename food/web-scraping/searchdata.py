f4ae6ae6dccb479b94beea868f4fe894777c0e5e
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-30 15:30:42 +0900

    fix : 스크래핑 코드 수정
    Former-commit-id: 069d6fcebba7a0906d07b8a97c76b8dd2d32fa89

--- food/web-scraping/searchdata.py
+++ food/web-scraping/searchdata.py
@@ -1,12 +1,3 @@
-import pandas as pd
-
-csv_file_path = 'searchData_ori.csv'
-output_csv_file = 'searchData_new.csv'
-
-# CSV 파일을 pandas로 읽기
-df = pd.read_csv(csv_file_path, encoding='cp949')
-
-# 데이터 정제하기
-df['title'] = df['title'].str.replace(r'\([^)]*\)|\[[^\]]*\]', '', regex=True)
-
-df.to_csv(output_csv_file, index=False, encoding='cp949')
+version https://git-lfs.github.com/spec/v1
+oid sha256:1caa6364b7d7fd56a35480f772a939f4addbf2e8076575be8ca6ddc21496d59e
+size 861
