e081f0653eb50f48ed5b51cc506ba24430c07d16
강현곤 <shoot_1@naver.com>
2023-10-30 12:58:59 +0900

    feat: hotelAPI
    Former-commit-id: bbea542394eef726ed423e6d8a130bd9810a1123

---
+++ hotel/hotelAPI/src/main/java/com/ssafy/i5i/hotelAPI/hotel/entity/Accommodation.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac3ce4ba29bec850406a0a79262306a3da882becafb8a857f8564812b246f401
+size 1271
---
+++ hotel/hotelAPI/src/main/java/com/ssafy/i5i/hotelAPI/hotel/entity/Attraction.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:550b365a4abf1a3e9b129871342b3f0e8a061aa8ab7bbbd6732f4a724b2a528f
+size 1345
