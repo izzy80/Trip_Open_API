cf4028fc991b6da9b1a33d730ab35c72816d81e3
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-30 15:36:25 +0900

    comment : index 제한한 거 주석처리
    Former-commit-id: dc86f040373178136faf549a7b4a541661dd3819

--- food/web-scraping/map.py
+++ food/web-scraping/map.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b1f958e5a1167a3f17d5afa9338a065c93095bcb0c9722b2e4d1eea18ba6615
-size 12200
+oid sha256:b3df7b8ad0e0e95b634fc2a4b3a69b81327701f6a797208b44b2699de570be40
+size 12202
