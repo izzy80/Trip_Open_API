d4b719f1c9d2abf1387acc23bd572e9f8ec43563
bro jjun <wjd5126@koreatech.ac.kr>
2023-10-31 11:05:15 +0900

    fext : error entity
    Former-commit-id: bcfd7e440b135b7fdb9779af7c85a514d1b9b662

--- hotel/hotelAPI/src/main/java/com/ssafy/i5i/hotelAPI/hotel/common/exception/ExceptionType.java
+++ hotel/hotelAPI/src/main/java/com/ssafy/i5i/hotelAPI/hotel/common/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b12fb5be8b64a8a61ef0a154ca43ed95966efa30d13c307c1320e697927f73bd
-size 4266
+oid sha256:2396fe886d23af06af2c336a22c83cbd4d1766685fb257e2d5954d6f93b9bfeb
+size 514
