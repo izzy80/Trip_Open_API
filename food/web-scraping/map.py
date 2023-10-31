89260e4ab305f68464adfd226320d146cf5fe784
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-31 15:37:27 +0900

    fix : csv 파일 저장 수정
    Former-commit-id: 20218573408b5b3cb7a4b6196c4ba460856773ed

--- food/web-scraping/map.py
+++ food/web-scraping/map.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:788a53a83d048b862a6b0b76ab38d5bc9d98b30c7d2b9c629f86377e5b62fd99
-size 12298
+oid sha256:b0f0649984384a5eacd934051a515ca2da42e4bd12bb093c3b818edad9f32ae0
+size 12888
