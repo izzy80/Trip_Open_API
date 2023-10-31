e0fed8ca354272e7f1754e22c5ba024ff6426871
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-10-31 10:03:21 +0900

    feat : 리눅스 코드 추가 및 변수명 수정
    Former-commit-id: 885f33c95708aaa0d12063e97831e639e8da1797

--- food/web-scraping/map.py
+++ food/web-scraping/map.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3df7b8ad0e0e95b634fc2a4b3a69b81327701f6a797208b44b2699de570be40
-size 12202
+oid sha256:db422dcaf7f6776eb501dc74b68122ec4b7fb5b5b09eba46b2b20a97f1cb7779
+size 12358
