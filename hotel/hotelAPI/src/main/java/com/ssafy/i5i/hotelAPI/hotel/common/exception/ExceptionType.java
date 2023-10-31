6bc073680d8886355c6b8c12924d548b08c9d108
bro jjun <wjd5126@koreatech.ac.kr>
2023-10-31 15:40:34 +0900

    feat : devlop sort
    Former-commit-id: fb7314d06a5de07fa4cb310572fc8696d9b73b1a

--- hotel/hotelAPI/src/main/java/com/ssafy/i5i/hotelAPI/hotel/common/exception/ExceptionType.java
+++ hotel/hotelAPI/src/main/java/com/ssafy/i5i/hotelAPI/hotel/common/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2396fe886d23af06af2c336a22c83cbd4d1766685fb257e2d5954d6f93b9bfeb
-size 514
+oid sha256:c0caca775be0b72100f7001a83fd50887d851d94142ada0514959d7cbe1d4b69
+size 589
