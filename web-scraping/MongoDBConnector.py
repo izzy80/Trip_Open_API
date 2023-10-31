7a97ca4477395eb5bdb1ac35f0fbfc132ccbcd6d
홍유빈 <acrow0330@naver.com>
2023-10-31 17:53:44 +0900

    feat: 관광지와 백과사전 정보 매핑
    Former-commit-id: bcf2d2fa2e85050db8156b901be08c7424e4676f

--- web-scraping/MongoDBConnector.py
+++ web-scraping/MongoDBConnector.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:998d34c220ebdb5e487ae8abc8954ee7e9078fdd44f09cb53840a48d55bdc2d4
-size 709
+oid sha256:9a6bf84e8df47c95f0aa43083212a31ee0f15806db74afafc8d707553358a8e3
+size 1030
