4ee2ca3f5b4bd87c26da113667fb542313a616ed
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-01 14:24:20 +0900

    fix : entity data type error ( long To int )
    Former-commit-id: 1d6e2e6056d2eff44bf42ec7c91abc5027eb7397

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/hotel/entity/Accommodation.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:17868bf52f96e16822e7506e0fb37b8cf13d1a3004895ebffd6a990a454f8682
-size 1728
---
+++ hotelAPI/src/main/java/com/ssafy/i5i/hotelAPI/hotel/entity/Accommodation.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b7a934b35e41517eb9796a6e281b4ebfb4c51e2ea7a26c1df327abbab6bd7d4
+size 1691
