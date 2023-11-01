01841968d04f491db900d7d36eeb436c0e628a49
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-01 12:53:02 +0900

    fix : 스크래핑 코드 네이버맵 지도 모바일로 수정
    Former-commit-id: 9efc1f0646058188c6eb37ab5b20e4a6ffd5b42d

--- pythonScript/food/searchdata.py
+++ pythonScript/food/searchdata.py
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1caa6364b7d7fd56a35480f772a939f4addbf2e8076575be8ca6ddc21496d59e
-size 861
+import pandas as pd
+
+csv_file_path = 'searchData_ori.csv'
+output_csv_file = 'searchData_new.csv'
+
+# CSV 파일을 pandas로 읽기
+df = pd.read_csv(csv_file_path, encoding='cp949')
+
+# 데이터 정제하기
+df['title'] = df['title'].str.replace(r'\([^)]*\)|\[[^\]]*\]', '', regex=True)
+df['naddr1'] = df['addr1'].str.split(' ').str[:2].str.join(' ')
+df['naddr2'] = df['addr1'].str.split(' ').str[:3].str.join(' ')
+
+df.to_csv(output_csv_file, index=False, encoding='cp949')
