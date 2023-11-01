5becccb2d9b36b0d8c4fd5cb2da469ed51738bb7
신창학 <csg1353@naver.com>
2023-11-01 17:28:18 +0900

    Feat : Spring Data Elasticsearch, @Mapping, @Setting, 가ㄴ다ㄴ한 CRUD
    Former-commit-id: 0eed202dfc0a7ac6de5312933b30c8fb7bdf8180

---
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/accommodation/dto/AccommodationDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:687a9329036a808ca84284997f50c5ad02c69bdfe37bb9aef7f18c8844e91ec8
+size 515
---
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/exception/ExceptionType.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9d72dc312a16faf72d0030735d73f6bd0cc91ac5b933067925ebf313061b17d
+size 649
