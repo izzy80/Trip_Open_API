fb4d57b15c3474f414e6b350e89498b00ee4fb3a
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-02 17:07:54 +0900

    fix : jwt error fix
    Former-commit-id: ef50e6375ae3bce63d434b32ccb3bd26a95b73b3

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5714f295b86e2cf61d0cd0ad2fd16f37c672c86c665bf07a5a8169c83b6ad80c
-size 3387
+oid sha256:f654f1020f54abb0c6ea74f7424b5122ab77f0b4b086e23219860786cbbbe4c6
+size 3393
--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/userSecurity/JwtAuthenticationProvider.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/userSecurity/JwtAuthenticationProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a39c19dbe075d55bae115ce88f5cc4c01f60b42b3e234f67b69bf44eae6d9950
-size 1587
+oid sha256:5cbec9885bd5af197c121fc0133ef4beff3c5a092a36e338f32bfe7234570f85
+size 1684
