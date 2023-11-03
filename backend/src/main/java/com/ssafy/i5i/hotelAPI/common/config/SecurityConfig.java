cd26becbef86dfa38d81611cc04e5bf01e706355
홍유빈 <acrow0330@naver.com>
2023-11-03 14:39:51 +0900

    feat: aop 설정
    Former-commit-id: b512d4cfbd85395a5d9ad5791d9244e9dc30e806

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3099634821adb5a7abefef647b4a4e33f1c39ad93d1a22e45b95023e95c2a9ab
-size 986
+oid sha256:7d8a19fa55118ea3f17c47b1ed1e5ca126aaa75a519ff914f12cdaa1369aa54e
+size 996
--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:253dcfab7805d3a6e12dc76008a0c152db233aad0a1be48eacb39141b6bebb64
-size 617
+oid sha256:ba13c00ae53409fcc5a92ed7b3e014fa0a22fb2fe5309154bebfb455c4d9b067
+size 1543
