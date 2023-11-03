a607d7706b261bad4180f3bacbb30ea6e3c4db5f
홍유빈 <acrow0330@naver.com>
2023-11-03 10:06:24 +0900

    feat: redis 설정
    Former-commit-id: ab9248a3291264f44c2cd8e57155217edf0ac559

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc415eaab29e4bd9a1952eea62b05a0dc080945b3d3a94e2f310aa341c75aae9
-size 646
+oid sha256:253dcfab7805d3a6e12dc76008a0c152db233aad0a1be48eacb39141b6bebb64
+size 617
