a4f7629069174c3e7e5588e4de051cd4c0673b4c
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-06 12:28:59 +0900

    fix : controller 수정
    Former-commit-id: 3b3dbda022b846838ac2eaa8fb90b3410653525f

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d229378128b5ac78d050ac9b4a3639c286afdd5aace1b7757ba69c20a50fab64
-size 1747
+oid sha256:caed8c16a0545fbe350cdee41df1a1983081ea052240cc684aa3d9080fbd7250
+size 1712
