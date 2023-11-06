725aa56899c6792cb973ea1374f16ac6b91c0b11
신창학 <csg1353@naver.com>
2023-11-06 18:00:36 +0900

    feat : springboot
    Former-commit-id: ca96457bd2ed0a9c660ea44e7ba993d9f313ced3

---
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/wiki/service/WikiServiceBasic.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4478f50cf8c293e2aa7eee6d1a17f58915e163df386e390c6a3c394f36fe9b21
+size 2326
---
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/wiki/service/WikiServiceExtend.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49c085bdfad31b8e7f3db4869e7270ee1fbc7cb4f76afd60ae66235bbd359fa0
+size 4541
