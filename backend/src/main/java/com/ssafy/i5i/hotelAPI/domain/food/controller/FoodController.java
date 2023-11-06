4ff644e4fc6fa0a80128615b4451ceff3bfa6380
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-06 14:15:15 +0900

    fix : hotel type error
    Former-commit-id: a465c93771c55bf12bd7972ff03d4724724b280d

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3dd2ff5ec7e3399a73269bd49892e5dfe526c1d1e2d42c32b3ebc4d72b1b108
+oid sha256:7d4abd6d033c4bae8e73ba791064771de7ecfa72cf109d9f2c0bb1507f7a5886
 size 1805
