2b3ead91aeabf41d9d074cc4dcfa2faa7012f882
홍유빈 <acrow0330@naver.com>
2023-11-07 17:54:28 +0900

    fix: synchronized로  멀티 스레드 환경에서 redis 동기화 설정
    Former-commit-id: 7a24429b4cae077e9398289dace32347344fef77

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca8b5e672dfbc3029c595fa4467a1d71fa72487d84dcf6cad2b62d3f5149a903
-size 2300
+oid sha256:670df83a9c4175683615fec9e5d079664afa770a81e6a5712ab9742f7f588d8a
+size 2387
