637b8fca54edf0f39ed3bed94547c30d0e4368b5
신창학 <csg1353@naver.com>
2023-11-08 17:59:07 +0900

    feat : add springboot ssl
    Former-commit-id: c0382590f0619c5d34733da4ccb251e72d0eb3ab

--- elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/accommodation/service/AccommodationService.java
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/accommodation/service/AccommodationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c898e17c94d619fb978ca26c5711e21edcbac4ed74e320e15055d12c47ea5d2
-size 1933
+oid sha256:c22115e6c23006e88c85d218d30afa0a9167749e2a8053d61d735b210c83a4e0
+size 1317
