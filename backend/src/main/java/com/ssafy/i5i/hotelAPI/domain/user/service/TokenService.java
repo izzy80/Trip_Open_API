4ddb237bdb0845fb14714aa1be61135cf071bf8d
홍유빈 <acrow0330@naver.com>
2023-11-08 15:31:19 +0900

    fix: redis 동기화 설정
    Former-commit-id: 592c8931fcb1455ac5b7a9afbbcc024ad7774880

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/user/service/TokenService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cb4ca101f4787ce6560323f02b03b5e1213f295cfc987d190a7ee4fd16ac94a
-size 2000
+oid sha256:58910fd1fc75ad355191c456e567ced8358178477570ea96d716ebbc08ef785c
+size 4761
