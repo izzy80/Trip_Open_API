3335a0ef299391ac6b84a039247371dcfb9c930e
신창학 <csg1353@naver.com>
2023-11-08 17:59:07 +0900

    feat : add springboot ssl
    Former-commit-id: e1dc4219ac18b0f983364a8f61517579d7fe6af0

--- elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/config/ElkConfig.java
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/config/ElkConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a300710e4780ab03ed8190248a3a723e6c2cb92fef4cf11c8d4573dabc42da1c
-size 1451
+oid sha256:827229be8454ba25ad3df4e4055ec8bb55eff7667fb4b32c6767cdbbf0e93cdd
+size 1708
--- elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/wiki/entity/Wiki.java
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/wiki/entity/Wiki.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af2b234da7e26401a5374c9abe4a79f4d50ebfc6ccd41a41dfc83b09a2a23607
+oid sha256:fb612bfe6f81069b1c5d13b4233d1995159355be34f20f2af52901b25b88e7a9
 size 992
