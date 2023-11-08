e190b7f952fcd46a624e2f0634795644a9248951
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-08 11:06:01 +0900

    fix : food service 수정
    Former-commit-id: feafbaf8a17659386251c3deea8ea45af48fc310

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/repository/FoodRepository.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/repository/FoodRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c798561558ca56fd084decb466b80697ac98c54d69efb8996ad057471b0e24b
-size 1572
+oid sha256:6609b7335f38a7942b98fc03cc69f518e45a8d608bdbb219f1e0c0cbc4a8ab0b
+size 1561
