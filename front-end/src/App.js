2fbf25278669bb9198db8ab0dda7769dbc8f37e9
이진호 <blueland1001@naver.com>
2023-11-08 17:17:57 +0900

    fix: 지정경로 외의 접근을 홈으로 리다이렉트
    Former-commit-id: b8aee78b7fc4dbdfb06caef1d192808708a6991f

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9c3613adc176562b0fe101290c94683fd6c7f1700f51ffea443ccbaae554630
-size 1672
+oid sha256:446e8ffba22d17d593f740f0f05f6f183573dd4cf6942f74f3c48b89c95e62da
+size 2355
