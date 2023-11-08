ada22d86fdf4e2bb3347f80cdc8a669a20ed98e1
홍유빈 <acrow0330@naver.com>
2023-11-08 17:39:52 +0900

    fix: token filter test 설정
    Former-commit-id: d2715f68e5247a3b34cd7a47e656edad6ed87114

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e2b5e8da46ffdd9a47f00847fd28c14d3497b10d19b9214bcbeda543a10d1b8
-size 3745
+oid sha256:c4dfa0e46e37795e006a667bf7066a46a13d8d9b711f05b1e1dfbc5e759d8d9c
+size 3757
