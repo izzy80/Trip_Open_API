04bca27eed6f49c8293c6bbc4bea3b0a89a54d90
홍유빈 <acrow0330@naver.com>
2023-11-08 17:08:07 +0900

    fix: token filter mysql로 동기 설정
    Former-commit-id: 0aecc884996544e525f47f880dec8587cd29f913

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/filter/ApiTokenCheckFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13281a37b22eec35081c7e057058f1905bb09274488a5b61fd87de1bc0caef57
-size 3743
+oid sha256:0e2b5e8da46ffdd9a47f00847fd28c14d3497b10d19b9214bcbeda543a10d1b8
+size 3745
