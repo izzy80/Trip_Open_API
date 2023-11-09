5cacafa3ffdd9a05ad069679b6dae54012a60ca8
이진호 <blueland1001@naver.com>
2023-11-09 13:53:58 +0900

    fix: 메인화면 문구 수정
    Former-commit-id: 46e93242ff0df31855cbc590b2dd8e9e43762002

--- front-end/src/components/main/Home.js
+++ front-end/src/components/main/Home.js
@@ -6,8 +6,9 @@ const Home = () => {
   return (
     <div className="Home">
       <div className="Home-body">
-        <pre className="Home-body-logo">SSAFY</pre>
-        <pre className="Home-body-logo">FINANCE</pre>
+        {/* <pre className="Home-body-logo">SSAFY</pre> */}
+        <pre className="Home-body-logo">여행지</pre>
+        <pre className="Home-body-logo">INFORMATION</pre>
         <pre className="Home-body-logo">OPEN API</pre>
       </div>
     </div>
