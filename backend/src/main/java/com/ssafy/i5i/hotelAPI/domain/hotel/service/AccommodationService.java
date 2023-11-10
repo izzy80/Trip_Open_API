158dcc879cf787b4b06d93c057d8507af03d4f5b
강현곤 <shoot_1@naver.com>
2023-11-10 15:44:21 +0900

    feat: hotelAPI name -> accommodation api logic change
    Former-commit-id: fefed5fed4ef031c938690f9f25e322c4e44ae6e

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/service/AccommodationService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/service/AccommodationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72af08987a3f84dc1c03a9364b71c82df497c3cca6597cbd2d14745fd4fc07ee
-size 5490
+oid sha256:41c14b77530ac79e52cba33b6f481adb9ad9e9e2e615726bb5b36d2fb261d707
+size 8219
