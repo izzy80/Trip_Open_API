a7f1078a1c2dd736842b4260e1ee1f3bf25ca23c
강현곤 <shoot_1@naver.com>
2023-11-10 10:23:50 +0900

    feat: attraction_name -> accommodation
    Former-commit-id: f2ad6c28eb7e278b5172e117af8c838b93ab3f56

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/repository/AccommodationRepository.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/repository/AccommodationRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42eef6e272bdfa96ef8884690291abee07ee8d40873aee9198bbb64cd5af35ed
-size 1416
+oid sha256:d9ce639cd5afbd69c42432afd23187f0ee6a2487db687816ae32e67b72a5c4e3
+size 1535
---
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/repository/AttractionAccommdodationRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:603060bb2784d2cdc011daa3e6b49275d1675df7ceaf0c78d91e1d5c3f17a9de
+size 864
