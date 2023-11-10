76055bbf751a2f4d000486a3a844dcb6d50f805b
강현곤 <shoot_1@naver.com>
2023-11-10 17:00:58 +0900

    feat: foodAPI, hotelAPI modify ~controller.java
    Former-commit-id: 35a438a5ab7eada05d2678311d9012b0ac510a27

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad6775ba95d910431b672ee4c32e6ba7f541ee04d0e00e52ff0ef58f74df7e21
-size 1637
+oid sha256:24221fb302f7f98a2ea9ce9600f72f0fdffc2e176a7d5c8375f6b1cf25b6b14d
+size 1388
--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/service/AccommodationService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/service/AccommodationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13e34809c33fbb919ce86c54b40c2fd950c87c1f7d1dc43fa9b967cf903a5354
+oid sha256:c8f4e78b2a344e402c00cf9dc25ce100c62d122ffb25191392f32272143110bc
 size 9236
