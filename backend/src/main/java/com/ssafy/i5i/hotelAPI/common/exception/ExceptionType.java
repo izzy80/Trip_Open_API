b20734655f8360b930294c896a90377cbd7809ec
강현곤 <shoot_1@naver.com>
2023-11-10 10:23:50 +0900

    feat: attraction_name -> accommodation
    Former-commit-id: cbb7fcfc7065244c64ba6c25e2fb5c8e19b5c4f3

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:825a09b1b3233eb6c8a342b270309e9db0c57d424463e08a64bdc2637e401f90
-size 873
+oid sha256:861e4a3be4b3a26c67e2be0641d3b0c17b1426539cf89309981e9b6136c97d4f
+size 868
