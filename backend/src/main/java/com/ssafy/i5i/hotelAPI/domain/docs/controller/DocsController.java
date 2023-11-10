7eda979ab71447812a3d50e577d411dd3afc1303
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-10 17:53:41 +0900

    fix : path
    Former-commit-id: fac8d9030825f1312e4586871d59f42041f1b6b0

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/controller/DocsController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/controller/DocsController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:942d0148dff6835425559de14eb13a2367ba4a38813b8276ea84470998de3ff5
-size 1061
+oid sha256:714aac8f860dcbd319d2d43c0e244c69c8c7a02ea9ad9322bc614f59d25addc0
+size 1072
