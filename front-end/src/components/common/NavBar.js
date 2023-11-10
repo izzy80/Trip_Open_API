f27c91d01cb735e0de49fae3bb10d8b5b3f0a808
이진호 <blueland1001@naver.com>
2023-11-10 11:01:20 +0900

    design: 푸터 바 디자인 변경
    Former-commit-id: 2195bcf14d54affb23c129bd7719f51999c28e45

--- front-end/src/components/common/NavBar.js
+++ front-end/src/components/common/NavBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecce754645589c1da79c30339f7f999ed2c383f9f381ffe19b2a6348cc0e0493
-size 2410
+oid sha256:63a7863d0a969911194e2c88cc5eef8910f4e317286eba4b931fefc6a27d9a28
+size 2429
