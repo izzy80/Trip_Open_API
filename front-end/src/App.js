df2086e9eca930272de4bdc8e9259eb304fb07e4
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-13 13:31:40 +0900

    feat : ServiceInfo 삭제 및 Elastic 소개 페이지 구현
    Former-commit-id: 19fa610339a023bbb841a7b2f8ccdee40d73f985

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2747fa8211cc0d391d200db44c27dd2d5c3b653513b0b1791cdf930423978d9
-size 2280
+oid sha256:c740e22222c2454e5e6b010595ca02a6ca7bd97cf75ae8e01379dc0394ef0dc9
+size 2221
