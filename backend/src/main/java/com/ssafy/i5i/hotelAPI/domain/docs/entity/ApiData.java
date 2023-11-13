87d8bc9487a4cd7b3156ddd89e33390858b33fd6
홍유빈 <acrow0330@naver.com>
2023-11-13 14:36:01 +0900

    feat: docs api 설정
    Former-commit-id: dcacc18c531bcd8c541db4f2fc4dd5cf33dfacf4

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiData.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:650dc47d9169a1298e423c54aa41e134aedd01478030e68d19d83d0ee1764ecb
-size 1666
+oid sha256:44c27382d3b9ed8ddb6ab956e8609e70c1fbedfcfa6ab82af089c212be5e65b2
+size 1083
--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/service/DocsService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/service/DocsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef6c19f8c1983b87951b7231f0cf4baea6196e4ce2208305ef699b79f2361c3c
-size 2140
+oid sha256:38b79ec4503a33a2d050008cf985c76c7278f39cf757da0b78ae604e2f1910f5
+size 2818
