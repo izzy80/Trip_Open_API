9e3d9819f02350c6cb231f3acac425d285654c8c
홍유빈 <acrow0330@naver.com>
2023-11-13 13:07:13 +0900

    fix: cors 에러 해결
    Former-commit-id: 4e022fb10775334b2d6fd0450ca0a08187b5152d

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ebd74cf0b429e0ec65d3ec783d341ce1f7aa70602b89b9ba839244164234fcd
-size 2957
+oid sha256:b109ffc47539a286fad4fbb3e12070d3efd10ae8c1a596d9159cf784900019f1
+size 3766
