3164e627ae47e3efde42583139e2527e7ada04e5
이진호 <blueland1001@naver.com>
2023-11-13 14:08:27 +0900

    fix: 회원가입 정보 수정
    Former-commit-id: b98e2d18ef3b7e4ea4451d6a56992adbd23af7a4

--- front-end/src/components/signup/Signup.js
+++ front-end/src/components/signup/Signup.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22b0e5908ab5b3e4e741ab43ddad18e125344fd775a7673be02b95988c7413b3
-size 5689
+oid sha256:9978d477ccd3a096625a043d1b84f43bb401df7fd4f3eb8d42285ad8c5a5ee50
+size 3706
