0ea72298ad245da25e59eefc2109f266f31de758
이진호 <blueland1001@naver.com>
2023-11-13 16:39:56 +0900

    fix : 초기 로딩 시 로그아웃 표시되는 오류
    Former-commit-id: 0447ec3c6aa26c4fbab9c2945416501f4494bde2

--- front-end/src/components/common/NavBar.js
+++ front-end/src/components/common/NavBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ea7bec4b90fa404a7a556f51d969d39263a033a1fef028c9f3597f23ad2d50b
-size 2330
+oid sha256:2274d4a825bf017ff1367b701f767d752b15141bbd5628e769e3e254429777ff
+size 2354
