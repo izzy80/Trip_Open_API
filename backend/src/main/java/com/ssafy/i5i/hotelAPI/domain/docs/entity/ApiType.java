04f0593585eccf95fec43d737d5360e157c338e9
홍유빈 <acrow0330@naver.com>
2023-11-13 23:40:10 +0900

    feat: 상위 카테고리에 대한 api 생성
    Former-commit-id: fe3db32413756e1fe44d461a303490c897d0ed56

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiType.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5025d39ad376b8a5cab59773b499df310f0644e505be9645ce1ffee2dccf8e08
-size 585
+oid sha256:6b5751d4d2b5276ae0637f2873cccb03e6169954df07825f0e90416533ee07b4
+size 705
--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/service/DocsService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/service/DocsService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94c93398ef5f62ccb6216db84bd06267165b2276264ca31fe5454a3689fb4ca3
-size 3938
+oid sha256:f8360b1757bc80b841916b30f8719cee4399511fb9787311b7dda7cfc9cf3cb6
+size 4518
