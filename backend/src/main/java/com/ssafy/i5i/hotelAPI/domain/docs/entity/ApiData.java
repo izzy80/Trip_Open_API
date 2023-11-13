c10150311692dbc04ebcb8af479895bb3dbb9e29
홍유빈 <acrow0330@naver.com>
2023-11-13 14:41:55 +0900

    fix: jpa 에서 설정
    Former-commit-id: 5577851e4e118fb93c7980fe7e5ada2d1db066d7

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiData.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44c27382d3b9ed8ddb6ab956e8609e70c1fbedfcfa6ab82af089c212be5e65b2
-size 1083
+oid sha256:a8463b97a3a8e5b73f5ab073f031621f9e154fd38e7f8b638e12dc31be09a813
+size 1090
