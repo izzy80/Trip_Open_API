0b572f4fca5bca8bb58c4254c47ef01707afc29e
홍유빈 <acrow0330@naver.com>
2023-11-13 17:59:17 +0900

    fix: docs controller 코드 정리
    Former-commit-id: c656e5113b3ef75e3b0ba7671031fdef95e32277

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/controller/DocsController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/controller/DocsController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b65c2e98bb42193c08fde5ac7ff4ff1205da306d821ae2542aee3df9f73351d
-size 1812
+oid sha256:a5499dea36953cc8c4a69318b480d45406aae17047df51a815214ef490b51d55
+size 1983
