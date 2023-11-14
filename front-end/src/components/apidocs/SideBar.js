610682aab4677ad9a8792e93ca7c838d5e2539c6
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-14 11:13:39 +0900

    feat : docs 페이지 axios
    Former-commit-id: 047e331edeac3baccfdfb6dffbdc760383612599

--- front-end/src/components/apidocs/SideBar.js
+++ front-end/src/components/apidocs/SideBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4c93b317de1f0516d905fb250abe4291c78052d3b81343943987f02cc4f7e5d
-size 3563
+oid sha256:afa011f312913a95a94f28ee7f3ca3ef895a0027b10fd8137828b0c443f2c2eb
+size 3220
