79a1ee145c2ff8492476ed7bd5eb7a3657c80b42
이진호 <blueland1001@naver.com>
2023-11-14 10:03:23 +0900

    fix: 버튼 오타수정
    Former-commit-id: 4a1ca0921532a49775f7aeb3cf77db35ca2ef6a3

--- front-end/src/components/common/NavBar.js
+++ front-end/src/components/common/NavBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3d099a2b90a4ce1c735dfb560eb8757072fad0defc6c9a1410f20f75694b1be
-size 2730
+oid sha256:07fab39d34b9544e179768f6d88f88706509a47474614b827ed1fdd0caa1cb02
+size 2705
