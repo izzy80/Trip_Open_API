b1e9a991a598d6f93dfdd10c9be2843fec9c9bdb
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-14 10:12:34 +0900

    feat : add api name in type entity and dto
    Former-commit-id: daef33f056a78065308c2287d7f4291b879ac267

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiType.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/docs/entity/ApiType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5751d4d2b5276ae0637f2873cccb03e6169954df07825f0e90416533ee07b4
-size 705
+oid sha256:5d3b28b607fd4354c16936699aa0f5307e542a14ee35b093a6b58fdaf6a68690
+size 737
