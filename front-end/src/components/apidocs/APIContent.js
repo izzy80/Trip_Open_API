93b9166260d4e57cb8cf7ef54a3db04b48812fd7
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-14 13:40:25 +0900

    design : api 문서 css
    Former-commit-id: 426b9668adb1e0ea145d58445d9cd14af2a2da66

--- front-end/src/components/apidocs/APIContent.js
+++ front-end/src/components/apidocs/APIContent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e98fdfb42b01575302203dd4341c6230776f1f1d467d44d003756222643d73ca
-size 4576
+oid sha256:bddaca48fb0edd9be14b07f753d19e8086e6fe5126da9b55091e04f0389090e6
+size 4639
