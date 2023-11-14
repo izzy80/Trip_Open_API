1cb5ccd05789ab4111599b01b1f240525859154d
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-14 17:23:16 +0900

    feat : add webClient test
    Former-commit-id: 02ee49fe3741ff813c4162a678cf6d757a1886c2

---
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5cef70dcf787828a36c4b325686de7db0e86de6027adc280ef954d41b92625f6
+size 563
---
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/service/ElasticService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2168dd147433c1cd36766c9e0170f3ea4bcd3067a0d78b0ac9a588306a9cae05
+size 706
