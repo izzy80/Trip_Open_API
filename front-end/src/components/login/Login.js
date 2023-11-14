e9bfd4e8e2a7994f104cadcc4b34497d0c43e795
강현곤 <shoot_1@naver.com>
2023-11-14 10:45:28 +0900

    feat:develop-Front
    Former-commit-id: c87402a98cf86db5eeea413e7e9c158ad428f529

--- front-end/src/components/login/Login.js
+++ front-end/src/components/login/Login.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7c4e5c552d708c059c31ac093a11ee0f8a8fee3587f9e1360f17b0cb1589769
-size 3231
+oid sha256:285377930540be24c4ec3d72b78678c36e6d7b5c8d3bb31a1edad22efff59732
+size 3250
