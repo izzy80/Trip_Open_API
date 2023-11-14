cd3732b08060c26f54ac9744000ab06e4c6415a4
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-14 16:57:11 +0900

    fix : food api coordi paramter 이름 수정
    Former-commit-id: 11fb1c683344e63dc4e8c9ca790e416be16b76f7

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c69250365cc5439956ef85bf0704e1185f6f5946e09308163cb25117f0da98f1
-size 1424
+oid sha256:eebfacd0368da4acb4274fa725b683193fb3e134f08b1aea7d99eef90521cf20
+size 1423
