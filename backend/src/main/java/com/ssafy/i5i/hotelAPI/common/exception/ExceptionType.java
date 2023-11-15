aba181229e0e65d287d52c3a5fab7fbde7735f64
홍유빈 <acrow0330@naver.com>
2023-11-15 16:14:49 +0900

    fix: email 인증 시 중복 메일 삭제
    Former-commit-id: 06e1f71823b562cd6368ce31a66b881a835d4431

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/common/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/common/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7023d1cf4f61fcb6e7cd96fe22ad71af8352d07b026f958b585de28d4ce5ef7c
-size 1983
+oid sha256:de6446967ab7028fed3c7ddbb52a6164d0ca35a61f0e8ca9140683cf0ecca818
+size 2013
