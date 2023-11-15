04c958da3579aac61eca32a06d1a550760b39873
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-15 11:09:41 +0900

    feat : entity to dto
    Former-commit-id: c80921ae60f004ed00f7428ced7325a401957c90

--- elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/wiki/controller/WikiController.java
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/wiki/controller/WikiController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73f091f138796d79ec182253b4ce1c524790c6cb7187d6ae12aa492460230344
-size 4655
+oid sha256:0f41a396a80f32163b297cd2d3d09eabdb54dce081193381fa9ce634bb46dd57
+size 4873
