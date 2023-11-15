a709d8f11cdeed3074306227c25f1c09d31d2967
홍유빈 <acrow0330@naver.com>
2023-11-15 23:41:57 +0900

    fix: food controller cors 에러 해결
    Former-commit-id: 28fdf65abf75640ef490e89046f437ce77282424

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/controller/FoodController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56afd10006fbe60d8efec9d10a5bcce7ac77d86764cbef69716b8384fd35107a
-size 1453
+oid sha256:6e86d59a7d2bf7d6d4c0695caeb27ef750881f3a0064078b128430d0a6f0b732
+size 1481
