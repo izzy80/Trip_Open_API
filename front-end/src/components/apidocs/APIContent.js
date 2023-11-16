892d3726ac278fe400a66e6e7d1421e6e0db29ce
강현곤 <shoot_1@naver.com>
2023-11-16 10:57:13 +0900

    Feat: APIContent.js 테스트쪽 수정
    Former-commit-id: eeecc3f4eb1028a7b2838c5f68e02ff353f20415

--- front-end/src/components/apidocs/APIContent.js
+++ front-end/src/components/apidocs/APIContent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e12687594599b82bf1e22ab2de02db75a8a3b70e78f106a088df99d39660e30
-size 13056
+oid sha256:6e61f5a2bb5b7128ac626c6e8cc8eb87e423c116c2a3f78a47175e35ec542f45
+size 13240
