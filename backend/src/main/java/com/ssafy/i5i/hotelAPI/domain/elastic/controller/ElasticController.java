802095ca6ae1bcd7389d1cc883a9608e0695fefc
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 13:30:33 +0900

    fix : searchAll post to get
    Former-commit-id: 7ad6081adf25cf21be4d68c361920ca031d951c2

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5de2a83d6dcffd2b6d91edcbf0f0489953ce7612a9222ea1441b28af91d796b
-size 5341
+oid sha256:67dac1262673760f819b4f81dac6709accbcb2540b076c48f77694a6a19bb641
+size 5471
