0a4036e8ede4dc976ee72ae6f231b1e9e5d6b1a6
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 09:31:28 +0900

    fix : Controller response type data To DataResponse
    Former-commit-id: 0aa826c578d2f744f73672e2f2047d1d0344cae6

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6b5875bedc17b4b86efbc5c070f2ba69eaf5ecbcb7d3649f8f37191c27ecf47
-size 4015
+oid sha256:91f4d0de4873a98fe574d58bf75f84569c1a71925be7608be24de9819a2bd175
+size 4750
--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/controller/AccommodationController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/controller/AccommodationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:668a4d58e4a9074788fc3f8ca157bb8abc0cf8e2effab1a4df88b8116bb763c9
-size 1773
+oid sha256:29b2bf87dcf313f05fd087c8efa8313722c93dc7d54939d3972130e4f093cb55
+size 2001
