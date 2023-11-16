16d6f675c7bc98d30facb277224907747758a01b
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 13:22:52 +0900

    fix : fix elastic search all
    Former-commit-id: 6ede3f725181ee8de6e16221525bc46777f7fa0f

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edd54f0ee0ec1badff2ac3f762270915ccca0a6d26972ab6525c00023b620271
-size 4944
+oid sha256:e5de2a83d6dcffd2b6d91edcbf0f0489953ce7612a9222ea1441b28af91d796b
+size 5341
