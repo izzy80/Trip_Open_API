3de5c79868f9a6b56289301231ec5c0933ef840e
이진호 <blueland1001@naver.com>
2023-11-16 17:51:46 +0900

    fix: 홈화면에서 보러가기 클릭 시 스크롤 최상단으로 강제이동
    Former-commit-id: 432679f4e9d68e984115666e90b7a75a87ef4e74

--- front-end/src/components/apidocs/APIContent.js
+++ front-end/src/components/apidocs/APIContent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eaa2207b26c5b708af05c6303b8daa8bd273d597e1b563ee508f7cba19e7a01
-size 11324
+oid sha256:57b7ecd960815beceba6e9f37e3f180bdb632292b3f9990b4466129c069a29f9
+size 11475
