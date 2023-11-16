46767111058a8ec030e87b9a92d1fb39f0aaa71f
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 23:07:25 +0900

    feat : add DataResponse
    Former-commit-id: 9d3d1a34af0705c5a6d8240272d3b56cd9d25c40

---
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/global/CommonResponse.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse {
+    private int code;
+    private String message;
+}
