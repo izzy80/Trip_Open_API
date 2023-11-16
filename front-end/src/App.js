4f677f8d90edaaaa1f89d47fc60bf1792afaa95d
이진호 <blueland1001@naver.com>
2023-11-16 10:59:51 +0900

    remove: 불필요 이미지파일 및 js파일 삭제
    Former-commit-id: aad2c070ea096b14c064bffba212f2276e01e47c

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc1f113ed0ee9bace563eda28b89f6b5b73a06ccf910d2c54657f615dcd0bd5c
-size 2287
+oid sha256:df038280586a4ace6819c5c38612426b8d1c4c8a950a0c46dce5af882201eeec
+size 1727
--- front-end/src/components/examplepage/ExCard.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:665d7372a6d3da96bd9f393f6dca96a2696e6c986f1d0723800d7f4885963deb
-size 1300
