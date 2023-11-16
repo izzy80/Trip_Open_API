ff42ad8724bbf8abd272212f34781071a853f0d9
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 10:42:28 +0900

    feat : add elastic null exception && add hotel, food 검색어 보정
    Former-commit-id: a53caf0b3ee73521edfa167824a82f26cf90de5f

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44eee6eec811fdf5eb444f95579d88f1147a87432ac3f58000d51a10e9ad4ce7
-size 6894
+oid sha256:dcbcb1e383dfb64feb89ef5e6a9dacc36a448ded22f381ac65938d7778ff3b0e
+size 7129
