cee48b739d7e3e009cebf3fc77f435c4ad23d681
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-16 23:01:03 +0900

    fix : sidebar 수정
    Former-commit-id: 4c163b299b86b18d603e86ad2e0c10c3d1ae8667

--- front-end/src/components/apidocs/SideBar.js
+++ front-end/src/components/apidocs/SideBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11109b60e6106e66ab1c12f0f8fd53d48da6e99f37e6ef7228f5c4dd74c2d08f
+oid sha256:37a25eee61c8d1a13738cc2227fac06d6cf58545e2ad2607ad25bb6f1ee2162c
 size 5364
