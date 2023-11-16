303843cc6196789706214735c29c1342d6a2098b
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 13:30:33 +0900

    fix : searchAll post to get
    Former-commit-id: 08dc453a9363c12a71f6fc796a3c8351eea4941e

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/service/ElasticService.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/service/ElasticService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84a7535ec2a0ea17bce9cf6109b1c8ee3878e42954d7f25304ed1d8825177e65
-size 7829
+oid sha256:be42fb03147203ddf591a0e7b53d2a5865d6ad86f1714ac48246b7dd405757e1
+size 7859
