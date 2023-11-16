7926655564c627e9eedf08d9ebb162d7d6c5a770
Jin <ljinho1001@gmail.com>
2023-11-16 23:17:23 +0900

    fix: 홈화면 검색API 보러가기 링크수정
    Former-commit-id: f509f7be5e4a2c1cb8d7a501249b91a8610d52dd

--- front-end/src/components/apidocs/SideBar.js
+++ front-end/src/components/apidocs/SideBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37a25eee61c8d1a13738cc2227fac06d6cf58545e2ad2607ad25bb6f1ee2162c
-size 5364
+oid sha256:62e29b139cff91c955f2a31327af5a32b677db0cb54fe010be7ad1d8d6486f91
+size 5442
