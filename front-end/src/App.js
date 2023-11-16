dfc46b034af9d968171cd1790070c7fc76217fc3
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-16 09:53:09 +0900

    feat : 홈화면 api docs로 연결 및 url 이상한 것 수정
    Former-commit-id: ca24395685f9ea28145006d886c369e49406fe30

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc1f113ed0ee9bace563eda28b89f6b5b73a06ccf910d2c54657f615dcd0bd5c
-size 2287
+oid sha256:e6ddf8fb741766bef88ac69594a5feb9bef3839df717be78d75d800e8d494e9d
+size 2319
