b1ef43d49fd4c38cff6bdd01b40446741488c769
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-16 15:07:40 +0900

    design : 글자 및 버튼 수정
    Former-commit-id: b0440705000a415c56e7f1dcf41adbe36d2b2a2e

--- front-end/src/components/apidocs/APIContent.js
+++ front-end/src/components/apidocs/APIContent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0713d0328d8048b2916bc3d8059ba535fd1fe20ae488b5b83b63edb90dacaa3
-size 13372
+oid sha256:403da5e58bf2614d261a8359b1b814b1092aed3ec2325c8fedcc400342d9cd12
+size 10635
