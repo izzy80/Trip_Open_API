a84ebbc8f1d8eab33d10937c2ecedd6b45d3c60d
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-16 23:58:49 +0900

    feat : input에 dummy data 미리 넣기
    Former-commit-id: 33f17b029924fc5232ae11670913bca1552adc3d

--- front-end/src/components/apidocs/SideBar.js
+++ front-end/src/components/apidocs/SideBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37a25eee61c8d1a13738cc2227fac06d6cf58545e2ad2607ad25bb6f1ee2162c
-size 5364
+oid sha256:cec10a7fe6d398c67a7a10fa165c25d2482709be73858bc4ded7026645853854
+size 5366
