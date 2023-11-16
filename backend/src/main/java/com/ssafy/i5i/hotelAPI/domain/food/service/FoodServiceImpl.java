8cf15419ed7aed2c498d24e0fd8f8ced8c6b26e5
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-16 22:24:55 +0900

    fix : add null pointer exception
    Former-commit-id: 140ad1a2d3428d910771782806d240d8bfc65371

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21d743582d3878e545056fde6ccb774fb0c3e41b5e56f7002ed567945b4ccc62
-size 7889
+oid sha256:8103d20f2b30d82d0bae62243db35a66a63660296edc6047f354282a6c3a8d46
+size 8069
