8904112b574fe4c84599b6c48e508c633d61af05
신창학 <csg1353@naver.com>
2023-11-16 13:37:16 +0900

    feat : food 제목검색 로직 확정
    Former-commit-id: 712b7c2c96e93c4784409c07eb1e44ea8f77efd4

--- elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/restaurant/service/RestaurantServiceBasic.java
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/restaurant/service/RestaurantServiceBasic.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ddf62bfd1191e22474e0924342d653d2cab5fa489a84afbc38bb9fc8b78b66a
-size 3159
+oid sha256:602017cfcf7f2cc2fb28cae33cf629921729f18e5b0ccf11ebefadd2ba89f9af
+size 2818
---
+++ elasticsearch-springboot/elasticsearch/src/main/java/com/sch/sch_elasticsearch/domain/restaurant/service/RestaurantTitleService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:240ea300e741f62bea5aa0cdca5408c5dcbcd26f38aec0ac182ad378c7da8b7a
+size 6394
