75e3ae987975efea1c73a2d4ffa9e911b8eb0730
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-17 00:45:50 +0900

    hotfix : elasitc controoller
    Former-commit-id: 0e944ad987912212f99479ac93a2fe87d686dc1c

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9e1550476ee1c7bcbac64557cad889b6c5baefbf4f2200ebd72c3ac670fe586
-size 5767
+oid sha256:3191f7ff5e4db795ceb830be5c81dfa2bd0599d8ac7e91bef9150bb3fe3dc522
+size 5517
