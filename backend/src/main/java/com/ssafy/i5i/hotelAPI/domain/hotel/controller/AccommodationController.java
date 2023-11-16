28d576c668a3612e6a339ce3e5f16066dc1178f7
bro jjun <wjd5126@koreatech.ac.kr>
2023-11-17 00:38:31 +0900

    hotfix : 롱티튜드 라티튜드 스왑
    Former-commit-id: cc3bf1c251382228df561763c6fc00494ee8e083

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/controller/AccommodationController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/hotel/controller/AccommodationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bf840d88c40a421f05fbf9ac739e030002dc59092e88b9ab4361d2bd2d5a0a0
+oid sha256:91b2541c35b6e63c9680a17c8b55fe808668342cea3e39447dac7ea7f84390ac
 size 2180
