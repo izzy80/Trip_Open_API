4b3476e13faf8203c00fa48ad161653a726eb2ff
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-17 15:17:20 +0900

    feat : sidebar 대분류 수정
    Former-commit-id: d34ec8534caa6c53114ebea36380db9263c5965f

--- front-end/src/components/apidocs/SideBar.js
+++ front-end/src/components/apidocs/SideBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a467109fd9c0b4d5e1a3333e683a7fad66188a8d426def2dec12661671897875
-size 5447
+oid sha256:81896cd9cd63c92cf0ab68fb9810ce087e4b5e73bedf14c3bfe74c6d3be3ddef
+size 5487
