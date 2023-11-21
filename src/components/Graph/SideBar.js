6044547757e77e543d79760a49848633439db514
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-21 16:55:43 +0900

    fix : graph 수정
    Former-commit-id: 46befa1f809f941195fe232d7606f5372b6afaa6

--- src/components/Graph/SideBar.js
+++ src/components/Graph/SideBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbf843527612afb9c52f2aaa0bb13d477e62807e54624f60705de876169e337c
-size 5437
+oid sha256:650a6522b176a774793afb324fe9b06d4027f891dfbaf97d9fd217a46edbcba1
+size 3693
