325f611316c9de309995f7d7273e2040b23c7f6e
izzy80 <115052929+izzy80@users.noreply.github.com>
2023-11-21 17:29:46 +0900

    fix : 대분류 이름 수정
    Former-commit-id: 10da1d5dd4c93b39e1c3af24d14b4c955329b0ed

--- src/components/Graph/SideBar.js
+++ src/components/Graph/SideBar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:650a6522b176a774793afb324fe9b06d4027f891dfbaf97d9fd217a46edbcba1
-size 3693
+oid sha256:89c4ca4545b29249f8722eaae8a532bec410717ba712cd84e8d86dbc2b672560
+size 3543
