9220444572b49237f341d24e77425d45bde7631f
홍유빈 <acrow0330@naver.com>
2023-11-21 13:27:41 +0900

    fix: db 변경
    Former-commit-id: 1b859dc9049ec80ffcd265e13ccc9eff7cdc5d82

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/food/service/FoodServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8103d20f2b30d82d0bae62243db35a66a63660296edc6047f354282a6c3a8d46
-size 8069
+oid sha256:e46f2b49516134af4012e5a40baa5e9f0579dea1074eb89de4a59db598a358f3
+size 8108
