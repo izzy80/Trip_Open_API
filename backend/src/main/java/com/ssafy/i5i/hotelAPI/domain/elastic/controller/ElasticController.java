de93425f319ca3e836195408c57431eb22b399d5
홍유빈 <acrow0330@naver.com>
2023-11-24 11:47:05 +0900

    fix: Elastic 요청 cors 에러 해결
    Former-commit-id: bdd0f9bb50209c7124d618382f7774e04ab68475

--- backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
+++ backend/src/main/java/com/ssafy/i5i/hotelAPI/domain/elastic/controller/ElasticController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3191f7ff5e4db795ceb830be5c81dfa2bd0599d8ac7e91bef9150bb3fe3dc522
-size 5517
+oid sha256:cf2ab77ab9c420dd6e4afd0f6e241e8ec6fd1b7051b08d21ce1425d122d3360e
+size 5545
